76bcabe6916201741977529a41aa5655dcb76230
ad <daniel.apatin@gmail.com>
2018-06-28 15:08:38 +0300

    * goreleaser

--- updater.sh
+++ updater.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-echo "$1"
-git tag "$1" && git push --tags
-go-bindata -pkg main -o bindata.go -prefix "data/" data/GeoLite2-City.mmdb
-gox -output="release/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64"
-rm bindata.go
-github-release release --user ad --repo gogeo --tag "$1" --name "$1"
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_darwin_386" --file release/gogeo_darwin_386
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_darwin_amd64" --file release/gogeo_darwin_amd64
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_linux_386" --file release/gogeo_linux_386
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_linux_amd64" --file release/gogeo_linux_amd64
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_linux_arm" --file release/gogeo_linux_arm
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_windows_386.exe" --file release/gogeo_windows_386.exe
-github-release upload --user ad --repo gogeo --tag "$1" --name "gogeo_windows_amd64.exe" --file release/gogeo_windows_amd64.exe
+oldversion=`git describe --tags \`git rev-list --tags --max-count=1\``
+newversion=`git describe --tags \`git rev-list --tags --max-count=1\` | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
+echo "${oldversion} -> ${newversion}"
+
+git tag "${newversion}" && git push --tags
+goreleaser
+rm -rf dist/
